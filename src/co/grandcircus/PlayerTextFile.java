f5fb1d4d987f8f47533048f32f4c03ead13e22e6
brandonbath <brandon_bath@yahoo.com>
2019-10-31 13:19:36 -0400

    Adding PlayerTextFile, and WordTextFile classes

--- src/co/grandcircus/PlayerTextFile.java
+++ src/co/grandcircus/PlayerTextFile.java
@@ -1,5 +1,64 @@
 package co.grandcircus;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class PlayerTextFile {
 
+	ArrayList<Player> Player = new ArrayList<>();
+
+	public static void createOurPlayerFile() {
+
+		String fileName = "Players.txt";
+		Path path = Paths.get(fileName);
+
+		if (Files.notExists(path)) {
+			try {
+				Files.createFile(path);
+				System.out.println("The file was created successfully");
+			} catch (IOException e) {
+				System.out.println("Something went wrong...file not created");
+			}
+		} else {
+			System.out.println("The file already exists!");
+		}
+
+	}
+
+	public static void writePlayers(ArrayList<Player> player) {
+
+		String fileName = "Players.txt";
+		Path path1 = Paths.get(fileName);
+
+		File file = path1.toFile();
+		PrintWriter output = null;
+
+		try {
+			output = new PrintWriter(new FileOutputStream(file, true));
+			for (Player p : player) {
+				output.println(p);
+				output.close();
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong.");
+		} finally {
+			output.close();
+		}
+	}
+
+	public ArrayList<Player> getPlayer() {
+		return Player;
+	}
+
+	public void setPlayer(ArrayList<Player> player) {
+		Player = player;
+	}
 }
--- src/co/grandcircus/WordTextFile.java
+++ src/co/grandcircus/WordTextFile.java
@@ -1,5 +1,65 @@
 package co.grandcircus;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+
+
 public class WordTextFile {
 
+	ArrayList<Word> getWords = new ArrayList<>();
+	
+	public static void createOurFile() {
+
+		String fileName = "Words.txt";
+		Path path = Paths.get(fileName);
+
+		if (Files.notExists(path)) {
+			try {
+				Files.createFile(path);
+				System.out.println("The file was created successfully");
+			} catch (IOException e) {
+				System.out.println("Something went wrong...file not created.");
+			}
+		} else {
+			System.out.println("The file already exists!");
+		}
+
+	}
+
+	public static void writeWord(Word word) {
+
+		String fileName = "Words.txt";
+		Path path1 = Paths.get(fileName);
+
+		File file = path1.toFile();
+		PrintWriter output = null;
+
+		try {
+			output = new PrintWriter(new FileOutputStream(file, true));
+			output.println(word);
+			output.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong.");
+		} finally {
+			output.close();
+		}
+
+	}
 }
+
+/*
+ * public ArrayList<Word> getGetWords() { return getWords; }
+ * 
+ * public void setGetWords(ArrayList<Word> getWords) { this.getWords = getWords;
+ * }
+ * 
+ */
